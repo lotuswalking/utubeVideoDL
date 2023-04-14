0958fcb5bb7d81c43d5d01089527540d9e0aeb27
junyan li <lijy1@lenovo.com>
2023-04-14 15:24:35 +0800

    add web server to access download request and

--- utubeVideToMp3.py
+++ utubeVideToMp3.py
@@ -1,28 +1,26 @@
-from pytube import YouTube
-from moviepy.editor import *
+from pytube import YouTube, helpers
+
 # pip install moviepy
 # pip install pytube
 # pip install python-vlc
 import os
-
-
-def convertToMp3(oldFileName):
-
-    newFileName = oldFileName.replace("mp4", "mp3")
-    video = VideoFileClip(oldFileName)
-    video.audio.write_audiofile(newFileName)
+from mymodule import *
 
 
 # https://www.youtube.com/watch?v=yoZJWWrGbi4
 # YouTube('https://www.youtube.com/watch?v=PjvU9xUw_XY').streams.first().download()
-url = 'https://www.youtube.com/watch?v='+'wgDYap0iJqk'
-yt = YouTube(url)
+url = 'https://www.youtube.com/watch?v='+'27Gqgxoka2o'
+newTitle = "莫問歸期"
+# proxy_handler = {
+#     "http": "10.62.220.124:9080",
+    
+# }
+# helpers.install_proxy(proxy_handler)
+yt = YouTube(url, proxies={'http': 'http://10.62.220.124:9080'})
 print(yt.streams.filter(type="video")[0])
 saveFolder = 'd:\\mp4\\'
-oldFileName = saveFolder + \
-    yt.title.replace(" ", "_").replace(":", "").replace(
-        "|", "").replace("-", "")+".mp4"
-oldFileName = oldFileName.replace("/", "")
+
+oldFileName = saveFolder + newTitle + ".mp4"
 # 下载mp4音频
 xx = yt.streams.filter(
     type="video", mime_type="video/mp4").order_by("abr").desc().first()
@@ -30,3 +28,4 @@ xx.download(output_path=saveFolder, skip_existing=True,
             filename=oldFileName)
 print("file "+oldFileName+" is done!")
 convertToMp3(oldFileName)
+# os.remove(oldFileName)
